9d59a8d71909e7955a893bfd44b13779663c6ec3
thojanssens <thojanssens1@gmail.com>
2021-06-20 20:29:03 +0200

    .

--- lib/use_case2.ex
+++ lib/use_case2.ex
@@ -3,4 +3,7 @@ defmodule MyApp.UseCase2 do
   # It needs to add changes such as errors.
 
   # For a list of changesets where some are to be :replace-d, no update is possible.
+
+  # It was solved though by adding an error in the parent changesets saying "some entries are unauthorized"
+  # even though slightly suboptimal
 end
